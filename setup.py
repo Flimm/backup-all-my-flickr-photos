449f1af3a8dc5b93ec5ec8d8f331023a1e61f5e1
David D Lowe <daviddlowe.flimm@gmail.com>
2016-07-14 13:22:09 +0100

    Release 0.2 with a better README.md

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 from setuptools import setup
 
+version='0.2'
+
 setup(
     name='backup_all_my_flickr_photos',
-    version='0.1',
+    version=version,
     description="A script to download all photos and videos in your Flickr account",
     author="David D Lowe",
     author_email="daviddlowe.flimm@gmail.com",
     url="https://github.com/Flimm/backup-all-my-flickr-photos",
-    download_url="https://github.com/Flimm/backup-all-my-flickr-photos/tarball/0.01",
+    download_url="https://github.com/Flimm/backup-all-my-flickr-photos/tarball/v%s" % (version,),
     scripts=['bin/backup-all-my-flickr-photos'],
     install_requires=[
         'flickr_api',
