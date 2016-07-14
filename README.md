c870ca10f20166a830be6358dab5d78c9dfc296d
David D Lowe <daviddlowe.flimm@gmail.com>
2016-07-14 12:39:00 +0100

    First version

---
+++ README.md
@@ -0,0 +1,4 @@
+# backup-all-my-flickr-photos
+
+This scripts simply downloads all your Flickr photos and videos into a directory.
+
---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='backup_all_my_flickr_photos',
+    version='0.01',
+    description="A script to download all photos and videos in your Flickr account",
+    author="David D Lowe",
+    author_email="daviddlowe.flimm@gmail.com",
+    script=['backup-all-my-flickr-photos'],
+    install_requires=[
+        'flickr_api',
+        'humanfriendly',
+    ],
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Environment :: Console',
+    ],
+)
